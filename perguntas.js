73393de28c77b88af93cdb05e57a36c97afe5dac
HiroshiDoi25 <hiroshi.doi@escola.pr.gov.br>
2025-10-10 08:00:10 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Progamação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital d França',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)

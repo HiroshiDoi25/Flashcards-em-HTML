e16f314defd56cb054c488ef21acd700b33d5c9e
HiroshiDoi25 <hiroshi.doi@escola.pr.gov.br>
2025-11-19 08:08:40 -0300

    crtc v

--- perguntas.js
+++ perguntas.js
@@ -1,20 +1,22 @@
 criaCartao(
-    'Progamação',
+    'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 criaCartao(
     'Geografia',
-    'Qual a capital d França',
+    'Qual a capital da França?',
     'A capital da França é Paris'
 )
+
 criaCartao(
-    'programação',
-    'O que é uma função',
+    'Programação',
+    'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
-    'Oi em ingles é Hi(RAI)'
+    'Oi em ingles é HI (RAI)'
+)
 )
